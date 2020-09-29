cb128b2a2d4ca84cfbd972f948bc4228dfa76d27
Antonio <antonio_97@live.se>
2020-09-29 19:45:31 +0200

    added admin auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const { ROLE_ADMIN } = require('../utils/constants');
 const config = require('config');
 
-module.exports = function (req, res, next) {
+function auth(req, res, next) {
   const token = req.header('x-auth-token');
   if (!token)
     return res
@@ -24,4 +25,40 @@ module.exports = function (req, res, next) {
     console.error(err);
     res.status(401).json({ msg: 'Server error' });
   }
+}
+
+function authAdmin(req, res, next) {
+  const token = req.header('x-auth-token');
+  if (!token)
+    return res
+      .status(401)
+      .json({ msg: 'Authentication token not found, authorization denied' });
+
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res
+          .status(401)
+          .json({ msg: 'Authentication token is not valid' });
+      }
+
+      const user = decoded.user;
+
+      if (user.role !== ROLE_ADMIN) {
+        return res.status(401).json({ msg: 'You are not authorized' });
+      }
+
+      req.user = user;
+      next();
+    });
+  } catch (err) {
+    console.error('Something went wrong with admin auth middleware: ');
+    console.error(err);
+    res.status(401).json({ msg: 'Server error' });
+  }
+}
+
+module.exports = {
+  auth,
+  authAdmin,
 };
