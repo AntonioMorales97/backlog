c22c012d5e21a0a429ad8797a79e6963784b15e1
Antonio <antonio_97@live.se>
2020-09-29 19:44:28 +0200

    added constant roles

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,3 +1,6 @@
 export const OPEN = 'OPEN';
 export const IN_PROGRESS = 'IN_PROGRESS';
 export const RESOLVED = 'RESOLVED';
+
+export const ROLE_ADMIN = 'ADMIN';
+export const ROLE_USER = 'USER';
--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,16 @@
+// Ticket Status
 const OPEN = 'OPEN';
 const IN_PROGRESS = 'IN_PROGRESS';
 const RESOLVED = 'RESOLVED';
+
+// Roles
+const ROLE_ADMIN = 'ADMIN';
+const ROLE_USER = 'USER';
 
 module.exports = {
   OPEN,
   IN_PROGRESS,
   RESOLVED,
+  ROLE_ADMIN,
+  ROLE_USER,
 };
